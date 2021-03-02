62d96a5fc723fcd02957439f8c500a60f2ed3891
d948521 <afsal.taj06@gmail.com>
2021-03-03 08:22:00 +1100

    for scalasyd

--- src/main/scala/com/thaj/zconfig/demo/Main.scala
+++ src/main/scala/com/thaj/zconfig/demo/Main.scala
@@ -5,6 +5,7 @@ package com.thaj.zconfig.demo
 import zio.config._
 import ConfigDescriptor._
 import zio.config.typesafe.TypesafeConfigSource
+import zio.config.gen._
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -29,10 +30,12 @@ object Main {
     val result: Either[ReadError[String], Unit] =
       for {
         jsonSource <- source
-        desc = ApplicationConfig.config from jsonSource
+        desc = ApplicationConfig.config from (jsonSource <> ConfigSource.fromMap(Map()))
         markdown = generateDocs(desc).toTable.toGithubFlavouredMarkdown
         _ = println(markdown)
+        _ = println(gen.generateConfigJson(desc, size = 10).unsafeRunChunk)
         result <- read(desc)
+        result2 = println(write(desc, result).map(_.flattenString()))
       } yield Core.run(result)
 
     println(result.swap.map(_.prettyPrint()))
